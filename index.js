b682cdc5085c5f2dc062645b3740ea18a420f0dd
hollowayjenelle <hollowayjenelle@gmail.com>
2022-10-11 16:33:25 -0500

    files updated and image added

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 let firstCard = 9
 let secondCard = 3
-
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById('message-el')
+let sumEl = document.querySelector('#sum-el')
+let cardsEl = document.getElementById('cards-el')
 
-if (sum <= 20){
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21){
-    console.log("Wohoo! You've ggot Blackjack!")
-}else{
-    console.log("You're out of the game")
+function startGame(){
+    renderGame()
 }
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " + cards[0] + ',' + cards[1]
+    sumEl.textContent = "Sum :" + sum
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    }else{
+        message = "You're out of the game"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+}
+
+function newCard(){
+    let card = 2
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+
+
+
+
